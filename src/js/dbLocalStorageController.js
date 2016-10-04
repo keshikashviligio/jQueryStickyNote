4931f342315e721fbdce24ab155e02cfe5f09d89
koco <keshikashviligio@gmail.com>
2016-10-04 16:15:41 +0400

    implement delete function is DbLocalStorageService

--- src/js/dbLocalStorageController.js
+++ src/js/dbLocalStorageController.js
@@ -13,26 +13,37 @@
         this.options = options;
     };
 
-
+    /**
+     * extend Base D
+     * @type {note.DbService}
+     */
     note.DbLocalStorageController.prototype = Object.create(note.DbController.prototype);
 
 
     note.DbLocalStorageController.prototype.constructor = note.DbLocalStorageController;
 
-
-    note.DbLocalStorageController.prototype.save = function (data) {
-         this._saveToLocalStorage(data)
+    /**
+     * add new note to localStorage
+     * @param newData
+     */
+    note.DbLocalStorageController.prototype.save = function (newData) {
+        var data = this._getAllFromLocalStorage();
+        data.push(newData);
+        this._saveToLocalStorage(data);
     };
 
-
+    /**
+     * delete note from localStorage
+     * @param id
+     */
     note.DbLocalStorageController.prototype.delete = function (id) {
-         var data = this._getAllFromLocalStorage();
-             for(var i = 0; i < data.length; i++){
-                 if(data[i].id == id){
-                     data.splice(data[i], 1);
-                 }
-             }
-             this._saveToLocalStorage(data);
+        var data = this._getAllFromLocalStorage();
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].id == id) {
+                data.splice(data[i], 1);
+            }
+        }
+        this._saveToLocalStorage(data);
     };
 
 
@@ -40,9 +51,18 @@
 
     };
 
-
-    note.DbLocalStorageController.prototype.update = function () {
-
+    /**
+     *
+     * @param updatedData
+     */
+    note.DbLocalStorageController.prototype.update = function (updatedData) {
+        var data = this._getAllFromLocalStorage();
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].id == updatedData.id) {
+                data[i] = updatedData;
+            }
+        }
+        this._saveToLocalStorage(data);
     };
 
 
@@ -50,30 +70,39 @@
 
     };
 
-
+    /**
+     *
+     * @returns {Array|Object}
+     */
     note.DbLocalStorageController.prototype.getData = function () {
         return this._getAllFromLocalStorage();
     };
 
-
-    note.DbLocalStorageController.prototype._addToLocalStorage = function (obj) {
-        var data = this._getAllFromLocalStorage();
-            data.push(obj);
-            this._saveToLocalStorage(data);
-    };
-
-
+    /**
+     *
+     * @returns {Array|object}
+     * @private
+     */
     note.DbLocalStorageController.prototype._getAllFromLocalStorage = function () {
         var storage = localStorage.getItem(this.options.localStorageKey);
         return storage ? JSON.parse(storage) : [];
     };
 
-
+    /**
+     *
+     * @param data
+     * @private
+     */
     note.DbLocalStorageController.prototype._saveToLocalStorage = function (data) {
         localStorage.setItem(this.options.localStorageKey, JSON.stringify(data));
     };
 
-
+    /**
+     *
+     * @param key
+     * @returns {*}
+     * @private
+     */
     note.DbLocalStorageController.prototype._getItemFromLocalStorage = function (key) {
         return this._getAllFromLocalStorage()[key];
     };
